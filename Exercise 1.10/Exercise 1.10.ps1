8fa66b6f939429399280d9c6d750edf6288d11ec
Islam <mdashraful.islam@savethechildren.org>
2023-06-01 11:52:56 +0600

    Exercise 1.9 & Exercise 1.10

---
+++ Exercise 1.10/Exercise 1.10.ps1
@@ -0,0 +1,1 @@
+PS C:\Users\mdashraful.islam> docker run -p 8081:8080 devopsdockeruh/simple-web-service server
---
+++ Exercise 1.9/Exercise 1.9.ps1
@@ -0,0 +1,1 @@
+PS C:\xampp\htdocs\devopswithdocker\Exercise 1.9> docker run -v "$(pwd)/logs:/usr/src/app/logs" devopsdockeruh/simple-web-service
